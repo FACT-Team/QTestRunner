742fcf1a20af5f1325bdab1afcb8afcb7c29a53e
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2015-03-16 14:31:00 +0100

    Hide tests who passed

--- testrunner.cpp
+++ testrunner.cpp
@@ -7,8 +7,10 @@ TestRunner &TestRunner::Instance() {
 
 int TestRunner::RunAll(int argc, char *argv[]) {
     int errorCode = 0;
+    QStringList lit;
+    lit << "" << "-silent";
     std::for_each( begin(m_tests), end(m_tests), [&] (QSharedPointer<QObject>& test) {
-        errorCode |= QTest::qExec(test.data(), argc, argv);
+        errorCode |= QTest::qExec(test.data(), lit);
         std::cout << std::endl;
     } );
 
